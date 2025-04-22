209f43d9bf68adbe40f73f85fe39505ad956ffa9
Tomás Díaz <tomas@bsv.direct>
2025-04-22 11:48:17 +0200

    starting point

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
-# Hypercore Detector
+# Spacecore Detector
 
-Detect the type of a Hypercore (Hypercore, Hyperbee or Hyperdrive)
+Detect the type of a Spacecore (Spacecore, Spacebee or Spacedrive)
 
 ## Install
 
-`npm i hypercore-detector`
+`npm i spacecore-detector`
 
 ## Usage
 
 ```
-const detect = require('hypercore-detector')
+const detect = require('spacecore-detector')
 
-let type = await detect(aHypercore)
+let type = await detect(aSpacecore)
 
 if (!type) {
-  type = detect(aHypercore, { wait: true })
+  type = detect(aSpacecore, { wait: true })
 }
 
 if (type === 'bee') {
@@ -22,19 +22,19 @@ if (type === 'bee') {
 } else if (type === 'drive') {
   console.log('I am the db core of a drive')
 } else {
-  console.log('I am a basic hypercore')
+  console.log('I am a basic spacecore')
 }
 
 ```
 
 ## API
 
-#### `const type = await detect(hypercore, opts?)`
+#### `const type = await detect(spacecore, opts?)`
 
-Detect the type of the `hypercore`.
+Detect the type of the `spacecore`.
 
 returns either `'core'`, `'bee'`, `'drive'` or `null`.
 
 `null` is returned when it is not yet possible to determine the type. Set `opts.wait = true` if instead you want to wait until the  type can be determined. 
 
-Do note that this will wait until the first block of the hypercore becomes locally available, so make sure you are either the writer, or are swarming on this core.
+Do note that this will wait until the first block of the spacecore becomes locally available, so make sure you are either the writer, or are swarming on this core.
--- test.js
+++ test.js
@@ -1,26 +1,26 @@
 const test = require('brittle')
 const RAM = require('random-access-memory')
-const Hypercore = require('hypercore')
-const Hyperbee = require('hyperbee')
-const Hyperdrive = require('hyperdrive')
-const Corestore = require('corestore')
+const Spacecore = require('bitspacecore')
+const Spacebee = require('spacebee')
+const Spacedrive = require('spacedrive')
+const Corestore = require('spacecorestore')
 
 const detect = require('./index.js')
 
-test('can detect normal hypercore', async (t) => {
-  const core = new Hypercore(RAM)
+test('can detect normal spacecore', async (t) => {
+  const core = new Spacecore(RAM)
   await core.ready()
   await core.append('I am just a normal core')
   t.is(await detect(core), 'core')
 })
 
 test('returns null if first block not available locally and wait not set', async (t) => {
-  const core = new Hypercore(RAM)
+  const core = new Spacecore(RAM)
   await core.ready()
   t.is(await detect(core), null)
 })
 
-test('returns null if first block not available locally and wait not set (hyperdrive)', async (t) => {
+test('returns null if first block not available locally and wait not set (spacedrive)', async (t) => {
   const drive = await getDrive()
   t.is(await detect(drive.db.core), null)
 })
@@ -28,7 +28,7 @@ test('returns null if first block not available locally and wait not set (hyperd
 test('waits for the first block if wait set', async (t) => {
   t.plan(1)
 
-  const core = new Hypercore(RAM)
+  const core = new Spacecore(RAM)
   await core.ready()
   detect(core, { wait: true })
     .then(res => t.is(res, 'core'))
@@ -37,7 +37,7 @@ test('waits for the first block if wait set', async (t) => {
   await core.append('I am just a normal core, but you only know this now')
 })
 
-test('waits for the first block if wait set (hyperdrive)', async (t) => {
+test('waits for the first block if wait set (spacedrive)', async (t) => {
   t.plan(1)
 
   const drive = await getDrive()
@@ -45,30 +45,30 @@ test('waits for the first block if wait set (hyperdrive)', async (t) => {
     .then(res => t.is(res, 'drive'))
     .catch(e => t.fail('Failed with unexpected error'))
 
-  await drive.put('/confirmed', 'I am a hyperdrive')
+  await drive.put('/confirmed', 'I am a spacedrive')
 })
 
-test('can detect a hyperbee', async (t) => {
-  const core = new Hypercore(RAM)
-  const bee = new Hyperbee(core)
-  await bee.put('Needs an entry', 'to officially be a hyperbee')
+test('can detect a spacebee', async (t) => {
+  const core = new Spacecore(RAM)
+  const bee = new Spacebee(core)
+  await bee.put('Needs an entry', 'to officially be a spacebee')
 
   t.is(await detect(core), 'bee')
   t.ok(core.closing == null, 'passed-in core not closed')
 })
 
-test('can detect a hyperdrive', async (t) => {
+test('can detect a spacedrive', async (t) => {
   const drive = await getDrive()
-  await drive.put('/file', 'needs a file to officially become a hyperdrive')
+  await drive.put('/file', 'needs a file to officially become a spacedrive')
 
   t.is(await detect(drive.db.core), 'drive')
 })
 
 async function getDrive () {
-  const corestore = new Corestore(RAM)
-  await corestore.ready()
+  const spacecorestore = new Corestore(RAM)
+  await spacecorestore.ready()
 
-  const drive = new Hyperdrive(corestore)
+  const drive = new Spacedrive(spacecorestore)
   await drive.ready()
 
   return drive
